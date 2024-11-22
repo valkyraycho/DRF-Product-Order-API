581720cb76a372c5bd33f6bd8ad0245b7ac57b6f
valkyraycho <razor0424@gmail.com>
2024-11-22 10:03:42 -0800

    update serializer fields

--- api/models.py
+++ api/models.py
@@ -41,8 +41,8 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="items")
     quantity = models.PositiveIntegerField()
 
     def __str__(self) -> str:
--- api/serializers.py
+++ api/serializers.py
@@ -17,14 +17,21 @@ class ProductSerializer(serializers.ModelSerializer[Product]):
 
 
 class OrderItemSerializer(serializers.ModelSerializer[OrderItem]):
+    product = serializers.StringRelatedField()
+    price = serializers.FloatField(source="product.price")
+
     class Meta:
         model = OrderItem
-        fields = ("product", "quantity")
+        fields = ("product", "quantity", "price", "item_subtotal")
 
 
 class OrderSerializer(serializers.ModelSerializer[Order]):
     items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ("order_id", "user", "created_at", "status", "items")
+        fields = ("order_id", "user", "created_at", "status", "items", "total_price")
+
+    def get_total_price(self, obj: Order) -> Decimal:
+        return sum(item.item_subtotal for item in obj.items.all())  # type: ignore
