52b1f5f79abc1753f6995f267ec55328c042cfb9
valkyraycho <razor0424@gmail.com>
2024-11-22 10:52:35 -0800

    update admin site

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemInline,)
+
+
+admin.site.register(Order, OrderAdmin)
