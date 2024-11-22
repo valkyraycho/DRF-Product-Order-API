a7abda5a383dfe43159f03a11b1ee554af854a71
valkyraycho <razor0424@gmail.com>
2024-11-22 10:03:21 -0800

    add order views

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.product_list_view),
-    path("products/<int:pk>/", views.product_detail_view),
+    path("products/", views.product_list_view, name="product-list"),
+    path("products/<int:pk>/", views.product_detail_view, name="product-detail"),
+    path("orders/", views.order_list_view, name="order-list"),
+    path("orders/<int:pk>/", views.order_detail_view, name="order-detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Order, Product
+from .serializers import OrderSerializer, ProductSerializer
 
 
 @api_view(["GET"])
@@ -18,4 +18,18 @@ def product_list_view(request: Request) -> Response:
 def product_detail_view(request: Request, pk: int) -> Response:
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def order_list_view(request: Request) -> Response:
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def order_detail_view(request: Request, pk: int) -> Response:
+    order = get_object_or_404(Order, pk=pk)
+    serializer = OrderSerializer(order)
     return Response(serializer.data)
